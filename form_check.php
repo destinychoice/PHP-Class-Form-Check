7f9628bad8f44cf7929ab911da7078b952c820df
destinychoice <jason@destinychoice.com.au>
2013-05-19 22:04:10 +0800

    Update form_check.php
    Added single level array handling
    Added password complexity check

--- form_check.php
+++ form_check.php
@@ -6,44 +6,64 @@
 		
 		A class designed to be a way to validate forms in a standard way to avoid human errors
 		
-		'rules' => 'required,email,number,telephone,mobile,postcode',
+		'rules' => 'required,email,number,telephone,mobile,postcode,password',
 	*/
 	class form_check {
-		
 		public $post_data;
 		public $error_page;
 		public $redirect;
-		
+
 		function __construct($user_post_data = NULL) {
 			$this->post_data = array();
 			$this->error_page = $_SERVER['HTTP_REFERER'];
 			$this->redirect = 0;
-			
+
 			if (is_array($user_post_data)) {
 				$this->post_data = $user_post_data;
-			} 
+			}
 		}
-		
+
 		function __destruct() {	/* Run any thing you need to when the class is deconstructed */	}
-		
-		function run_validation() {	
+
+		function run_validation() {
 			if ($this->post_data == array() || empty($this->post_data)) {
 				exit("<pre>Post data is empty please correct.</pre>");
 			}
-			
+
+			/* HANDLE ARRAYS */
+			foreach ($this->post_data as $key => $row) {
+				if (is_array($row['value'])) {
+					foreach ($row['value'] as $i => $value) {
+						$count = $i + 1;
+						$this->post_data += array(
+							"{$key}{$i}" => array(
+								'name' => "{$row['name']} {$count}",
+								'rules' => $row['rules'],
+								'value' => $value
+							)
+						);
+
+					}
+				}
+			}
+
 			foreach ($this->post_data as $key => $row) {
 				/* TEST FOR BLANK REQUIRED FIELD */
 				if (preg_match("/required/", $row['rules']) && empty($row['value'])) {
 					$_SESSION['temp']['error'][$key] = "$row[name] cannot be blank";
 				}
 				/* DO THE FOLLOWING CHECKS ONLY IF NOT EMPTY */
-				if (!empty($row['value'])) {
+				if (!empty($row['value']) AND !is_array($row['value'])) {
 					/* TEST FOR EMAIL FIELDS */
 					if (preg_match('/email/', $row['rules']) && !preg_match("/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$/i", $row['value'])) {
 						$_SESSION['temp']['error'][$key] = "$row[name] is an invalid email";
 					}
 					/* TEST FOR NUMBER FIELDS */
 					if (preg_match('/number/', $row['rules']) && preg_match('/[^0-9]/', $row['value'])) {
+						$_SESSION['temp']['error'][$key] = "$row[name] can only contain numbers";
+					}
+					/* TEST FOR FLOAT FIELDS */
+					if (preg_match('/float/', $row['rules']) && preg_match('/[^0-9\.]/', $row['value'])) {
 						$_SESSION['temp']['error'][$key] = "$row[name] can only contain numbers";
 					}
 					/* TEST FOR TELEPHONE FIELDS */
@@ -58,21 +78,25 @@
 					if (preg_match('/postcode/', $row['rules']) && (preg_match('/[^0-9]/', $row['value']) || $row['value'][3] == '')) {
 						$_SESSION['temp']['error'][$key] = "$row[name] seems to be an invalid postcode";
 					}
+					/* TEST FOR PASSWORD COMPLEXITY FIELDS */
+					if (preg_match('/password/', $row['rules']) && (!preg_match('/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6})/', $row['value']))) {
+						$_SESSION['temp']['error'][$key] = "$row[name] must contain at least one upper case letter, one lower case letter and one digit and be at least six characters long";
+					}
 				}
 			}
 		}
-		
+
 		function redirect() {
 			if ($this->error_page == NULL) {
 				exit("<pre>Error page is empty please correct.</pre>");
 			}
-		
+
 			if ($_SESSION['temp']['error'] && $this->redirect == 1) {
 				header('Location: ' . $this->error_page);
 				exit;
-			}			
+			}
 		}
-		
+
 		// HELPER
 		// Creates a array of any fields in the $_post to make it easy to build the validation array
 		function generate_validation_list() {
